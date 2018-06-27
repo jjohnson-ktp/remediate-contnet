48c445cdf356fe82e52b04e542388ce4deebd04c
Jamal Johnson <jamal.johnson@kaplan.com>
2018-06-27 13:51:40 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  content: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getContent().subscribe((res) => {
+      console.log(res);
+      this.content = res;
+    });
+  }
+
+  getContent() {
+    return this.http.get('https://content-integration.qa.ktp.io/api/v1/items/remediation_13_sample');
+  }
+
 }
